c6e23f4ffba53884c471950f01190628c55c447c
david lobser <davidlobser@yahoo.com>
2017-11-30 14:13:08 -0700

    render texture resolution 2k and tweaks to default planet size

--- Assets/Balls/Scripts/SceneUtil.cs
+++ Assets/Balls/Scripts/SceneUtil.cs
@@ -8,6 +8,7 @@ public class SceneUtil : MonoBehaviour {
 	public GameObject[] planes;
 	public Texture[] tex;
 	public Texture GridTex;
+	public float speed = 1;
 	// Use this for initialization
 	void Start () {
 		tex = new Texture[planes.Length];
@@ -27,6 +28,17 @@ public class SceneUtil : MonoBehaviour {
 		}
 		if (Input.GetKeyUp (KeyCode.R)) {
 			SceneManager.LoadScene (0);
+		}
+		if (Input.GetKey (KeyCode.Alpha7)) {
+			Transform g = Camera.main.transform;
+			g.localEulerAngles = new Vector3 (g.localEulerAngles.x + Input.GetAxis ("Mouse Y") * speed, g.localEulerAngles.y + Input.GetAxis ("Mouse X") * speed, g.localEulerAngles.z);
+		}
+		if (Input.GetKey (KeyCode.Alpha8)) {
+			Camera.main.fieldOfView = Camera.main.fieldOfView + Input.GetAxis ("Mouse Y") * speed;
+		}
+		if (Input.GetKey (KeyCode.Alpha9)) {
+			Transform g = Camera.main.transform;
+			g.localPosition = new Vector3 (g.localPosition.x, g.localPosition.y + Input.GetAxis ("Mouse Y") * speed*.1f, g.localPosition.z);
 		}
 	}
 }
